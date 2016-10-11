b84d6daf431011dac4468683743f50f709d49bc7
LioBouchan <leo.bouchan@gmail.com>
2016-10-11 16:26:53 -0500

    Falta la implementación del boton y obtener los datos que se necesitan para el número maravilloso

--- NumeroMaravilloso/app/src/main/java/liobouchan/numeromaravilloso/MainActivity.java
+++ NumeroMaravilloso/app/src/main/java/liobouchan/numeromaravilloso/MainActivity.java
@@ -2,12 +2,24 @@ package liobouchan.numeromaravilloso;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editTextNumero;
+    private Button botonMaravilloso;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Elementos del código iguales a los elementos del XML
+        editTextNumero = (EditText)findViewById(R.id.editTextNumero);
+        botonMaravilloso = (Button)findViewById(R.id.buttonMaravilloso);
+
+        //Implementación del OnCLickListener
+
     }
 }
